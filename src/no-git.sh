ead9fb06dc477897b0f3c21383685e52f9a271a7
philcryer <phil@philcryer.com>
2019-06-18 14:16:53 -0500

    added an option to use if user does not have git installed yet

---
+++ src/no-git.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+set -e
+
+distro=$(uname)
+
+function msg_status () {
+    echo -e "\x1B[01;34m[*]\x1B[0m $1"
+}
+function msg_good () {
+    echo -e "\x1B[01;32m[*]\x1B[0m $1"
+}
+function msg_error () {
+    echo -e "\x1B[01;31m[*]\x1B[0m $1"
+}
+function msg_notification () {
+    echo -e "\x1B[01;33m[*]\x1B[0m $1"
+}
+
+msg_status "let's (auto) basejump!"
+
+os-check(){
+    msg_notification "checking operating system"
+        if [ "$distro" == "Linux" ]; then
+            msg_good "$distro is supported, continuing"
+        elif [ "$distro" == "MacOS" ]; then
+            msg_good "$distro is supported, continuing"
+        fi
+}
+
+software-check(){
+  msg_notification "checking for required software"
+    for cli in curl python pip git sudo; do
+      if ! type "$cli" > /dev/null 2>&1; then
+        msg_error "$cli is not installed - install that and try again"; exit 1
+      fi
+  	done
+  msg_good "all required software installed"
+}
+
+get-basejump(){
+  msg_notification "checking out basejump-master"
+    if [ ! -d '$HOME/devel' ]; then
+        msg_notification "creating checkout directory"
+        mkdir -p $HOME/devel
+    fi
+    if [ -d '$HOME/devel/basejump-master' ]; then
+        rm -rf $HOME/devel/basejump-master
+        cd $HOME/devel; curl -sL https://github.com/philcryer/basejump/archive/master.tar.gz | tar xz
+    else
+        cd $HOME/devel; curl -sL https://github.com/philcryer/basejump/archive/master.tar.gz | tar xz
+    fi
+    msg_good "basejump-master downloaded"
+}
+
+main(){
+    os-check;
+    software-check;
+    get-basejump;
+    msg_status "edit gitconfig vars and run basejump!"
+}
+
+main;
+
+cd $HOME/devel/basejump-master
+pwd
+
+exit 0
